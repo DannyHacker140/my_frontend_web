3de59f52a498119535a2db19db2c63c7238bec12
DannyHacker140 <dannyhacker140@gmail.com>
2025-02-20 20:48:57 +0100

    Update contact.js to redirect

--- contact.js
+++ contact.js
@@ -3,10 +3,17 @@ function sendMail() {
         from_name: document.getElementById("name").value,
         from_email: document.getElementById("email").value,
         message: document.getElementById("message").value,
-        };
-        emailjs.send('service_zly0h3i', 'template_ecs3owe', params)
+    };
+
+    emailjs.send('service_zly0h3i', 'template_ecs3owe', params)
         .then(function(res) {
-            alert('Message sent successfully!', res.status) 
+            alert('Message sent successfully!');
+            // Redirect to the homepage after the alert is closed
+            window.location.href = "index.html";
         })
+        .catch(function(error) {
+            alert('Failed to send the message. Please try again.');
+            console.error('Error:', error);
+        });
 }
   
